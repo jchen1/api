faf6a375f568fe96c85e9b11c941316fe293b8eb
Jeff Chen <jchen@ladderlife.com>
2020-05-27 15:40:14 -0700

    refactor cron

--- apiserver/src/cron.ts
+++ apiserver/src/cron.ts
@@ -8,76 +8,125 @@ import { ICronHandler } from "./types.ts";
 // forked from https://deno.land/x/cron/cron.ts
 // -------------------------------- BEGIN FORK --------------------------------
 
-export class Cron {
-  cronJobs: { schedule: string; fn: void }[];
-  constructor() {
-    this.cronJobs = [];
-  }
-  add = (schedule: string, fn: any): void => {
-    this.cronJobs.push({
-      schedule: schedule,
-      fn: fn,
-    });
-  };
+type CronJob = {
+  schedule: string;
+  fn: () => any;
+};
 
-  matches = (cronPart: string, currPart: number): boolean => {
-    const opts = cronPart.split(",").map(p => p.trim());
+const days = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+const months = [
+  "jan",
+  "feb",
+  "mar",
+  "apr",
+  "may",
+  "jun",
+  "jul",
+  "aug",
+  "sep",
+  "oct",
+  "nov",
+  "dec",
+];
 
-    return opts.some(part => {
-      if (cronPart === "*") return true;
-      if (cronPart === String(currPart)) return true;
+const cronParts: Record<string, (date: Date) => number> = {
+  minute: date => date.getMinutes(),
+  hour: date => date.getHours(),
+  dayOfMonth: date => date.getDate(),
+  month: date => date.getMonth() + 1,
+  dayOfWeek: date => date.getDay(),
+};
 
-      const rangeMatch = part.match(/(\d+)-(\d+)/);
-      if (rangeMatch !== null) {
-        const start = parseInt(rangeMatch[1]);
-        const end = parseInt(rangeMatch[2]);
-        return start <= currPart && currPart <= end;
-      }
+function cronPartMatches(now: Date, cronPart: string, part: string) {
+  const currPart = cronParts[part](now);
+  // A list is a set of numbers (or ranges) separated by commas.
+  const opts = cronPart.split(",").map(p => p.trim());
 
-      const stepMatch = cronPart.match(/\*\/(\d+)/);
-      if (stepMatch !== null) {
-        const step = parseInt(stepMatch[1]);
-        return currPart % step === 0;
-      }
+  return opts.some(part => {
+    // A field may contain an asterisk, which always stands for "first-last".
+    if (cronPart === "*") return true;
+    // exact match
+    if (cronPart === String(currPart)) return true;
+    // Names can also be used for the 'month' and 'day of week' fields.
+    // Use the first three letters of the particular day or month
+    // (case does not matter)
+    if (part === "month" && cronPart.toLowerCase() === months[currPart + 1])
+      return true;
+    if (part === "dayOfWeek" && cronPart.toLowerCase() === days[currPart])
+      return true;
 
-      return false;
-    });
-  };
+    const rangeMatch = part.match(/(\d+)-(\d+)/);
+    const stepMatch = cronPart.match(/[\d-*]+\/(\d+)/);
 
-  validate = (schedule: string): boolean => {
-    const now = new Date();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
-    const dayOfMonth = now.getDate();
-    const month = now.getMonth() + 1;
-    const dayOfWeek = now.getDay();
+    let rangeMatches = true;
+    let stepMatches = true;
 
-    const crontab = schedule.split(" ");
-    const currTime = [minutes, hours, dayOfMonth, month, dayOfWeek];
+    let rangeStart: number | undefined;
+    let rangeEnd: number | undefined;
 
-    return currTime.every((currPart, idx) =>
-      this.matches(crontab[idx], currPart)
-    );
-  };
-
-  filterJobs = () => {
-    let result = [];
-    for (let i = 0; i < this.cronJobs.length; i++) {
-      if (this.validate(this.cronJobs[i].schedule)) {
-        result.push(this.cronJobs[i].fn);
-      }
+    if (rangeMatch !== null) {
+      rangeStart = parseInt(rangeMatch[1]);
+      rangeEnd = parseInt(rangeMatch[2]);
+      rangeMatches = rangeStart <= currPart && currPart <= rangeEnd;
     }
-    return result;
+
+    // Step values can be used in conjunction with ranges.
+    // Following a range with "/<number>" specifies skips of the
+    // number's value through the range.
+    if (stepMatch !== null) {
+      const step = parseInt(stepMatch[1]);
+      return (currPart - (rangeStart || 0)) % step === 0;
+    }
+
+    return rangeMatches && stepMatches;
+  });
+}
+
+function timeForCron(now: Date, schedule: string) {
+  const crontab = schedule.split(" ");
+
+  const matches = Object.keys(cronParts).reduce((acc, k, idx) => {
+    acc[k] = cronPartMatches(now, crontab[idx], k);
+    return acc;
+  }, {} as Record<string, boolean>);
+
+  // Note: The day of a command's execution can be specified in the following
+  // two fields --- 'day of month', and 'day of week'.
+  // If both fields are restricted (i.e., do not contain the "*" character),
+  // the command will be run when either field matches the current time.
+  return (
+    matches.minute &&
+    matches.hour &&
+    matches.month &&
+    (matches.dayOfMonth || matches.dayOfWeek)
+  );
+}
+
+export class Cron {
+  jobs: CronJob[];
+
+  constructor() {
+    this.jobs = [];
+  }
+
+  add = (schedule: string, fn: () => any) => {
+    if (
+      !schedule.match(/((?:[\d*-/]+|[A-Za-z]{3}) ){4}(?:[\d*-/]+|[A-Za-z]{3})/)
+    ) {
+      throw new Error(`invalid crontab: ${schedule}!`);
+    }
+    this.jobs.push({ schedule, fn });
   };
 
   async start() {
-    const jobs: any[] = this.filterJobs();
-    setTimeout(() => {
-      this.start();
-    }, (61 - new Date().getSeconds()) * 1000);
-    for (let i = 0; i < jobs.length; i++) {
-      await jobs[i]();
-    }
+    const now = new Date();
+    setTimeout(() => this.start(), (61 - now.getSeconds()) * 1000);
+
+    return Promise.allSettled(
+      this.jobs
+        .filter(({ schedule }) => timeForCron(now, schedule))
+        .map(job => job.fn())
+    );
   }
 }
 
