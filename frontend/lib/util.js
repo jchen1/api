e0df9864cb8a66169972f742f8df60203c92f21d
Jeff Chen <jchen@ladderlife.com>
2020-06-17 09:28:30 -0700

    delete frontend

--- frontend/lib/util.js
+++
@@ -1,7 +0,0 @@
-export function prettifyData(data) {
-  return isNaN(data) ? data : +data.toFixed(2);
-}
-
-export function last(arr, def = null) {
-  return arr.length > 0 ? arr[arr.length - 1] : def;
-}
