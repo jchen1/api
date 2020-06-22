f5d0606f74f44cc5414f2b0ce873312f58575978
Jeff Chen <jchen@ladderlife.com>
2020-06-22 08:27:08 -0700

    chrome-extension: add source option

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -3,27 +3,30 @@
 const apiURL = "https://api.jeffchen.dev/";
 
 function sendEvent(urlOrId, event) {
-  chrome.storage.sync.get({ token: "" }, ({ token }) => {
-    if (token !== "" && urlOrId !== "") {
-      const data = {
-        event,
-        source: {
-          major: "chrome-extension",
-          minor: "v0.1",
-        },
-        type: typeof urlOrId === "string" ? "text" : "int",
-        data: urlOrId,
-      };
-      return fetch(apiURL, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(data),
-      });
-    }
-  });
+  chrome.storage.sync.get(
+    { token: "", source: "v0.1" },
+    ({ token, source }) => {
+      if (token !== "" && urlOrId !== "") {
+        const data = {
+          event,
+          source: {
+            major: "chrome-extension",
+            minor: source,
+          },
+          type: typeof urlOrId === "string" ? "text" : "int",
+          data: urlOrId,
+        };
+        return fetch(apiURL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(data),
+        });
+      }
+    },
+  );
 }
 
 chrome.tabs.onCreated.addListener(({ url }) => sendEvent(url, "visited_url"));
@@ -34,7 +37,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 });
 
 chrome.tabs.onActivated.addListener(({ tabId, windowId }) => {
-  chrome.tabs.get(tabId, tab => {
+  chrome.tabs.get(tabId, (tab) => {
     sendEvent(tab.url, "switched_tab");
   });
 });
@@ -43,13 +46,13 @@ chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
   sendEvent(tabId, "closed_tab");
 });
 
-chrome.windows.onCreated.addListener(window =>
+chrome.windows.onCreated.addListener((window) =>
   sendEvent(window.id, "opened_window")
 );
-chrome.windows.onRemoved.addListener(windowId =>
+chrome.windows.onRemoved.addListener((windowId) =>
   sendEvent(windowId, "closed_window")
 );
-chrome.windows.onFocusChanged.addListener(windowId => {
+chrome.windows.onFocusChanged.addListener((windowId) => {
   if (windowId >= 0) {
     sendEvent(windowId, "switched_window");
   }
--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -1,32 +1,36 @@
 // Saves options to chrome.storage
 function save_options() {
-  var token = document.getElementById("token").value;
+  const token = document.getElementById("token").value;
+  const source = document.getElementById("source").value;
+
   chrome.storage.sync.set(
     {
       token,
+      source,
     },
     function () {
       // Update status to let user know options were saved.
-      var status = document.getElementById("status");
+      const status = document.getElementById("status");
       status.textContent = "Options saved.";
       setTimeout(function () {
         status.textContent = "";
       }, 750);
-    }
+    },
   );
 }
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get(
     {
       token: "",
+      source: "",
     },
     function (items) {
       document.getElementById("token").value = items.token;
-    }
+      document.getElementById("source").value = items.source;
+    },
   );
 }
 document.addEventListener("DOMContentLoaded", restore_options);
